b99ab799ce5d75ce07f599e327ae897525f27cd9
LondonEnergyMap <dora.ma@etude.co.uk>
2019-03-20 18:33:59 +0000

    added scripts to generate graphs exploring building parameters and retrofit interventions, and a simple regression model

---
+++ need/need.py
@@ -0,0 +1,171 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+# filepath to CLEANED need data
+filepath = 'https://raw.githubusercontent.com/LondonEnergyMap/cleandata/master/need_ldn.csv'
+
+# read csv into dataframe
+df_all = pd.read_csv(filepath)
+
+# select data in year 2012
+y = 2012
+
+df = df_all[df_all.year==2012]
+
+# ---------------------
+# make regression graphs for main parameters
+
+cols = ['imd_eng', 'age', 'proptype', 'floorarea_band', 'epc_band', 'gcons', 'econs']
+
+xaxisnames = ['IMD', 'age', 'property type', 'floor area band', 'EPC band']
+
+# temporary dataframe for plotting graphs
+t = df[cols]
+temp = t.copy()
+
+# shift age and proptype to range 1-6 rather than 101-106
+temp['age'] = temp['age'] - 100
+temp['proptype'] = temp['proptype'] - 100
+
+# maximum consumption limits (from cleaning data)
+maxgcons = 50000
+maxecons = 25000
+
+cols_len = len(temp.columns)
+
+# omit the last 2 columns
+k = 2
+
+# number of subplots to include according to columns length
+nplot = cols_len-k 
+
+# create figure with a number of subplots for gas
+f, axes = plt.subplots(1, nplot, figsize=(3*nplot, 3))
+
+ymin = -5000
+
+for i in range(nplot):
+    xmax = len(temp[cols[i]].unique()) +1
+
+    sns.regplot(data=temp, x=cols[i], y='gcons', scatter_kws={'alpha':0.1}, ax=axes[i])
+
+    axes[i].set_xlim([0, xmax])
+    axes[i].set_ylim([ymin, maxgcons-ymin])
+    axes[i].set_ylabel('annual gas consumption (kWh)')
+    axes[i].set_xlabel(xaxisnames[i])
+    if i!=0:
+        axes[i].get_yaxis().set_ticks([])
+        axes[i].set_ylabel('')
+
+
+plt.subplots_adjust(left=0.07, bottom=0.2, right=0.95, top=0.9, wspace=0.2, hspace=0.2)
+
+plt.savefig('need2012gas_regplots.png')
+
+# ---------------------
+
+# create figure with a number of subplots for electricity
+f, axes = plt.subplots(1, nplot, figsize=(3*nplot, 3))
+
+ymin = -2500
+
+for i in range(nplot):
+    xmax = len(temp[cols[i]].unique()) +1
+
+    sns.regplot(data=temp, x=cols[i], y='econs', scatter_kws={'alpha':0.1}, ax=axes[i])
+
+    axes[i].set_xlim([0, xmax])
+    axes[i].set_ylim([ymin, maxecons-ymin])
+    axes[i].set_ylabel('annual electricity consumption (kWh)')
+    axes[i].set_xlabel(xaxisnames[i])
+    if i!=0:
+        axes[i].get_yaxis().set_ticks([])
+        axes[i].set_ylabel('')
+
+
+plt.subplots_adjust(left=0.07, bottom=0.2, right=0.95, top=0.9, wspace=0.2, hspace=0.2)
+
+plt.savefig('need2012elec_regplots.png')
+
+# ---------------------
+
+cols2 = ['mainheatfuel', 'walls', 'cwi', 'loftins', 'boiler', 'loftdepth', 'gcons', 'econs']
+
+xaxisnames2 = ['gas / other', 'cavity wall / other', 'wall insulation', 'loft insulation', 'new boiler','loft depth']
+
+# temporary dataframe for plotting graphs
+t = df[cols2]
+temp = t.copy()
+
+# fill NaN as 0 
+temp = temp.fillna(value=0)
+
+dfloftdepth = df[df.loftdepth!=99]
+
+nplot = len(xaxisnames2)
+
+# ---------------------
+# create figure with a number of subplots for gas
+f, axes = plt.subplots(1, nplot, figsize=(3*nplot, 3))
+
+ymin = -5000
+
+
+for i in range(nplot):
+    xmax = temp[cols2[i]].max() +1
+    xmin = temp[cols2[i]].min() -1
+            
+    if cols2[i]=='loftdepth':
+        sns.regplot(data=dfloftdepth, x=cols2[i], y='gcons', scatter_kws={'alpha':0.1}, ax=axes[i])
+        xmax = dfloftdepth[cols2[i]].max() +1
+    else:
+        sns.regplot(data=temp, x=cols2[i], y='gcons', scatter_kws={'alpha':0.1}, ax=axes[i])
+
+    axes[i].set_xlim([xmin, xmax])
+    axes[i].set_ylim([ymin, maxgcons-ymin])
+    axes[i].set_ylabel('annual electricity consumption (kWh)')
+    axes[i].set_xlabel(xaxisnames2[i])
+    if i!=0:
+        axes[i].get_yaxis().set_ticks([])
+        axes[i].set_ylabel('')
+    
+
+
+plt.subplots_adjust(left=0.07, bottom=0.2, right=0.95, top=0.9, wspace=0.2, hspace=0.2)
+
+plt.savefig('need2012gas_regplots2.png')
+
+# ---------------------
+# create figure with a number of subplots for electricity
+f, axes = plt.subplots(1, nplot, figsize=(3*nplot, 3))
+
+ymin = -2500
+
+
+for i in range(nplot):
+    xmax = temp[cols2[i]].max() +1
+    xmin = temp[cols2[i]].min() -1
+            
+    if cols2[i]=='loftdepth':
+        sns.regplot(data=dfloftdepth, x=cols2[i], y='econs', scatter_kws={'alpha':0.1}, ax=axes[i])
+        xmax = dfloftdepth[cols2[i]].max() +1
+        xmin = dfloftdepth[cols2[i]].min() -1
+    else:
+        sns.regplot(data=temp, x=cols2[i], y='econs', scatter_kws={'alpha':0.1}, ax=axes[i])
+
+    axes[i].set_xlim([xmin, xmax])
+    axes[i].set_ylim([ymin, maxecons-ymin])
+    axes[i].set_ylabel('annual electricity consumption (kWh)')
+    axes[i].set_xlabel(xaxisnames2[i])
+    if i!=0:
+        axes[i].get_yaxis().set_ticks([])
+        axes[i].set_ylabel('')
+    
+
+
+plt.subplots_adjust(left=0.07, bottom=0.2, right=0.95, top=0.9, wspace=0.2, hspace=0.2)
+
+plt.savefig('need2012elec_regplots2.png')
---
+++ need/need_interventions.py
@@ -0,0 +1,110 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+
+def main():
+
+    # filepath to CLEANED need data
+    filepath = 'https://raw.githubusercontent.com/LondonEnergyMap/cleandata/master/need_ldn.csv'
+
+    # read csv into dataframe
+    df_all = pd.read_csv(filepath)
+
+    interventions = ['cwi', 'loftins', 'boiler']
+    titles = ['Cavity wall insulation', 'Loft insulation', 'New boiler']
+    leglabels = ['before', 'during', 'after']
+
+    xyear = [x+'_year' for x in interventions]
+    
+    # ------------
+    for i in range(len(interventions)):
+        temp_inter = df_all[['hid', interventions[i], xyear[i], 'gcons', 'econs', 'year']]
+        temp = temp_inter[temp_inter[interventions[i]]==1]
+        temp['xhue'] = temp.apply(colorgen, xyear=xyear[i],  axis=1) 
+
+        d = {x: i for i, x in enumerate(set(temp.hid))}
+        temp['hid2'] = [d[x] for x in temp['hid']]
+        l = len(temp.hid2.unique())
+
+        # analysis of effectiveness of the intervention
+        temp_eff = temp.groupby(['hid2', 'xhue']).mean()
+        temp_eff = temp_eff[['gcons']]
+        t = temp_eff.unstack()
+        t.columns = t.columns.droplevel(level=1)
+        t.columns = leglabels
+        t = t.dropna()
+        t['dif'] = t.after - t.before
+        t['eff'] = np.where(t.dif<0, '1', '0')
+        nhouses = len(t.index)
+        t['eff'] = pd.to_numeric(t.eff)
+        eff_percent = t.eff.sum()/nhouses
+        print(eff_percent)
+        
+        # plot and save scatter graphs for different interventions
+        plt.figure(figsize=(25, 5))
+        g = sns.scatterplot(data=temp, x='hid2', y='gcons', hue='xhue')
+        plt.xlim([-1,l+1])
+        plt.xlabel('Household ID')
+        plt.ylabel('annual gas consumption (kWh)')
+        plt.title(titles[i] + 'effectiveness = %0.1f%%' % (eff_percent*100))
+
+        legend = g.legend()
+        handles, labels = g.get_legend_handles_labels()
+        g.legend(handles=handles[1:], labels=leglabels, title='Period relative to intervention')
+
+        plt.savefig(interventions[i]+'.png')
+        
+        # zoom into 100 houses only and save
+        plt.xlim([-1,101])
+        plt.savefig(interventions[i]+'_zoom100.png')
+
+        # create distribution graphs of gas consumption based on effectiveness
+        t_eff = t[t.eff==1]
+        t_neff = t[t.eff==0]
+        
+        plt.figure()
+        sns.kdeplot(t_eff.before, shade=True)
+        sns.kdeplot(t_neff.before, shade=True)
+        plt.legend(['effective', 'not effective'])
+        plt.xlabel('annual gas consumption (kWh)')
+        plt.title(titles[i] + ' - consumption before intervention')
+        plt.savefig(interventions[i]+'_before.png')
+
+        plt.figure()
+        sns.kdeplot(t_eff.before, shade=True, color='r')
+        sns.kdeplot(t_eff.after, shade=True, color='g')
+        plt.xlabel('annual gas consumption (kWh)')
+        plt.title(titles[i] + ' - before and after effective intervention')
+        plt.savefig(interventions[i]+'_eff.png')
+
+        plt.figure()
+        sns.kdeplot(t_neff.before, shade=True, color='r')
+        sns.kdeplot(t_neff.after, shade=True, color='g')
+        plt.xlabel('annual gas consumption (kWh)')
+        plt.title(titles[i] + ' - before and after NON-effective intervention')
+        plt.savefig(interventions[i]+'_neff.png')
+
+        plt.figure()
+        sns.kdeplot(t_eff.before, shade=True)
+        sns.kdeplot(t_eff.after, shade=True)
+        sns.kdeplot(t_neff.before, shade=True)
+        sns.kdeplot(t_neff.after, shade=True)
+        plt.legend(['effective before', 'effective after', 'not effective before', 'not effective after'])
+        plt.xlabel('annual gas consumption (kWh)')
+        plt.title(titles[i] + ' - before and after intervention')
+        plt.savefig(interventions[i]+'_both.png')
+
+def colorgen(df, xyear):
+    c = 100 # error value
+    if df.year > df[str(xyear)]:
+        c = 1
+    if df.year < df[str(xyear)]:
+        c = -1
+    if df.year == df[str(xyear)]:
+        c = 0
+    return c
+
+# call main body of code
+if __name__=='__main__':
+    main()
